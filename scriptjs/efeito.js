2c58874049e0d2fdb06ce5331d9c8e7a1e45752b
Gabriel Lima Carvalho <gabriel.lima.tinfem.2022@gmail.com>
2024-08-30 14:32:14 -0300

    resumo

---
+++ scriptjs/efeito.js
@@ -0,0 +1,14 @@
+ function   clicqui() {
+
+    let  img  = document.getElementById('imagem'); 
+    
+    if (img.style.display === 'none') { 
+
+        img.style.display = 'block'; 
+    } else
+     {
+        img.style.display = 'none';}
+ }
+
+
+
